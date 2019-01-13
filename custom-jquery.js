81fb5d2065e4a432336eb9ce1a27c31659b3596c
Sachin <sk963451@gmail.com>
2019-01-13 19:49:25 +0530

    jquery commit changes

--- custom-jquery.js
+++ custom-jquery.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
          $("#myScrollspy ul li.active").next().removeClass("visited");
         }  
       }); 
-      $("#myScrollspy a").click(function(){
-        var activeClass= $("#myScrollspy ul").find('.active');        
-        $("#myScrollspy ul li.active").addClass("visited");
-        var nextVisited= $("#myScrollspy ul li.active").next().hasClass("visited");
-        if(nextVisited){
-         $("#myScrollspy ul li.active").nextAll().removeClass("visited");
-        }  
-      });   
+      // $("#myScrollspy a").click(function(){
+      //   var activeClass= $("#myScrollspy ul").find('.active');        
+      //   $("#myScrollspy ul li.active").addClass("visited");
+      //   var nextVisited= $("#myScrollspy ul li.active").next().hasClass("visited");
+      //   if(nextVisited){
+      //    $("#myScrollspy ul li.active").nextAll().removeClass("visited");
+      //   }  
+      // });   
   });
