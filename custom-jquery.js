db85bc4be5f1e139828fe1502c5449245e5bc21a
Sachin <sk963451@gmail.com>
2019-01-22 23:11:57 +0530

    clicked functionality added for stepper

--- custom-jquery.js
+++ custom-jquery.js
@@ -1,19 +1,27 @@
-
-$(document).ready(function(){   
-    $(window).scroll(function(){
-        var activeClass= $("#myScrollspy ul").find('.active');        
-        $("#myScrollspy ul li.active").addClass("visited");
-        var nextVisited= $("#myScrollspy ul li.active").next().hasClass("visited");
-        if(nextVisited){
-         $("#myScrollspy ul li.active").next().removeClass("visited");
-        }  
-      }); 
-      // $("#myScrollspy a").click(function(){
-      //   var activeClass= $("#myScrollspy ul").find('.active');        
-      //   $("#myScrollspy ul li.active").addClass("visited");
-      //   var nextVisited= $("#myScrollspy ul li.active").next().hasClass("visited");
-      //   if(nextVisited){
-      //    $("#myScrollspy ul li.active").nextAll().removeClass("visited");
-      //   }  
-      // });   
+var flag='hello';
+// console.log(flag);
+$(document).ready(function(){         
+        $(window).scroll(function(){                  
+            if(flag!=='nothello')  {
+                // console.log("scoll enter");
+                var activeClass= $("#myScrollspy ul").find('.active').addClass("visited");        
+                var nextVisited= $(activeClass).next().hasClass("visited");
+                if(nextVisited){                    
+                $(activeClass).next().removeClass("visited");
+                }                 
+            } 
+            else{
+              flag='hello';
+            }           
+          });    
   });
+  
+  function clickFunction(current) {
+      // console.log("enter click");
+    flag='nothello';           
+    $(current).parent().addClass("visited");
+    var nextVisit= $(current).parent().next().hasClass("visited");
+    if(nextVisit){
+     $(current).parent().nextAll().removeClass("visited");
+    }    
+  }
